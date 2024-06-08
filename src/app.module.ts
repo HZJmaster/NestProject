486524a54b0c68bc3d5384052c5af17773978814
HZJMaster <846163332@qq.com>
2024-06-09 01:20:19 +0800

    feat(all): 完成发送邮箱功能!

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,33 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './modules/cats.module';
+import { MailerModule } from '@nest-modules/mailer';
+import { PugAdapter } from '@nest-modules/mailer/dist/adapters/pug.adapter';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { EmailModule } from './modules/email/email.module';
+import * as path from 'path';
 
+// dbjcgfmowakfbehg
 @Module({
-  imports: [CatsModule],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: 'smtps://846163332@qq.com:dbjcgfmowakfbehg@smtp.qq.com',
+        defaults: {
+          from: 'scholarlyServe<846163332@qq.com>',
+        },
+        template: {
+          dir: path.join(__dirname, './templates/email'),
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+    CatsModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/email/email.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nest-modules/mailer';
+
+@Injectable()
+export class EmailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  sendEmail() {
+    this.mailerService.sendMail({
+      to: '3555474128@qq.com',
+      from: '846163332@qq.com',
+      subject: 'I want to send this email for you!',
+      template: 'welcome',
+    });
+  }
+}
