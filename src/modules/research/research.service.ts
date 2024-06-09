a2ff8d0671509c4bcc6544136617c4d0ba6dc4f7
HZJMaster <846163332@qq.com>
2024-06-10 00:24:24 +0800

    添加事务的实例

--- src/modules/research/research.service.ts
+++ src/modules/research/research.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { ResearchEntity } from './research.entity';
 
 @Injectable()
@@ -8,6 +8,7 @@ export class ResearchService {
   constructor(
     @InjectRepository(ResearchEntity)
     private researchRepository: Repository<ResearchEntity>,
+    private dataSource: DataSource,
   ) {}
 
   async findAll(): Promise<ResearchEntity[]> {
@@ -20,5 +21,25 @@ export class ResearchService {
 
   async create(ResearchName: string): Promise<ResearchEntity | null> {
     return await this.researchRepository.save({ ResearchName });
+  }
+
+  async createMany(ResearchNames: string[]) {
+    // 使用事务
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      ResearchNames.forEach(async (item) => {
+        await queryRunner.manager.save(item);
+      });
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      // since we have errors lets rollback the changes we made
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // you need to release a queryRunner which was manually instantiated
+      await queryRunner.release();
+    }
   }
 }
